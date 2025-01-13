47e6b5ccf98bb6a70d92569b6608a57ba028d2df
virattt <virat.dot@gmail.com>
2025-01-13 17:23:01 -0500

    Update homepage URL

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -30,6 +30,8 @@ export function AppSidebar({ user }: { user: User | undefined }) {
             <Link
               href="/"
               onClick={() => {
+                // Go to financialdatasets.ai
+                window.location.href = 'https://financialdatasets.ai';
                 setOpenMobile(false);
               }}
               className="flex flex-row gap-3 items-center"
